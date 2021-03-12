526c20e9e1e7d1000b3d3bd385195dd54e2c53c3
Oren Mendelow <om144@ilab2.cs.rutgers.edu>
2021-03-12 00:56:12 -0500

    renamed main file to ww.c, created README, removed inputprocessor from Makefile, added project submission files to folder and compressed that folder into a .tar

--- oldww.c
+++ oldww.c
@@ -1,19 +1,53 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
-#include <dirent.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <ctype.h>
+#include <unistd.h>
 #include <fcntl.h> 
 #include <assert.h>
+
+
+int wrap(unsigned width, int input_fd, int output_fd);
+
+
+int main(int argc, char* argv[]) {
+
+    if (argc < 2 || argc > 3) {
+        perror("Wrong number of arguments!");
+        exit(EXIT_FAILURE);
+    }
+    int width = atoi(argv[1]);
+    assert(width > 0);
+    // if (width < 1) {
+    //     perror("Width must be positive!");
+    //     exit(EXIT_FAILURE);
+    // }
+    if (argc == 2) {
+        int error = wrap(width, 0, 1);
+        if (error == 1) {
+            exit(EXIT_FAILURE);
+        }
+    }
+    else if (argc == 3) {
+        int fd = open(argv[2], O_RDONLY);
+        if (fd == -1) {
+            perror(argv[2]);
+            exit(EXIT_FAILURE);
+        }
+        int error = wrap(width, fd, 1);
+        if (error == 1) {
+            exit(EXIT_FAILURE);
+        }
+    }
+        
+    return EXIT_SUCCESS;
+}
 
 int wrap(unsigned width, int input_fd, int output_fd) {
 
     int error = 0; //variable to check if wordLength > width
     int bytes;
-    int size = 256*sizeof(char); //buffer size
+    int size = 128*sizeof(char); //buffer size
     char *buffer = malloc(size);
     char *word = NULL; //stores partial word from the end of buffer
     int start = 0;  //starting index of a word
@@ -22,7 +56,6 @@ int wrap(unsigned width, int input_fd, int output_fd) {
     int characters = 0; //number of characters in a line
     int newLine = 0; //variable to indicate if a newline needs to be inserted
     while ((bytes = read(input_fd, buffer, size)) > 0) {
-		// printf("length: %d, width: %d\n", (characters+ wordLength), width);
         start = 0;
         end = 0;
         //second case of new line: last element of buffer was \n
@@ -40,7 +73,7 @@ int wrap(unsigned width, int input_fd, int output_fd) {
             if (wordLength > width) {
                 write(output_fd, "\n", 1);
                 write(output_fd, word, wordLength);
-                characters += wordLength;
+                write(output_fd, "\n", 1);
                 error = 1;
             }
             else if (characters + wordLength <= width) {
@@ -67,7 +100,7 @@ int wrap(unsigned width, int input_fd, int output_fd) {
         else if (!isspace(buffer[start]) && word != NULL) {
             while (!isspace(buffer[end])) {
                 end++;
-                if (end >= bytes) {
+                if (end == bytes) {
                     break;
                 }
                 //check for 2 newlines in a row
@@ -95,7 +128,7 @@ int wrap(unsigned width, int input_fd, int output_fd) {
                 if (wordLength > width) {
                     write(output_fd, "\n", 1);
                     write(output_fd, word, wordLength);
-                    characters += wordLength;
+                    write(output_fd, "\n", 1);
                     error = 1;
                 }
                 else if (characters + wordLength <= width) {
@@ -117,15 +150,22 @@ int wrap(unsigned width, int input_fd, int output_fd) {
                 start = end + 1;
                 end = start;
             }
-            else if (end >= bytes) {
+            else if (end == bytes) {
                 continue;
             }
         }
+
+        // if (buffer[0] == '\n' && buffer[1] == '\n') {
+        //     characters = 0;
+        //     write(output_fd, "\n", 1);
+        //     write(output_fd, "\n", 1);
+        //     newLine = 0;
+        // }
         
         while (end != bytes) {
             while (!isspace(buffer[end])) {
                 end++;
-                if (end >= bytes) {
+                if (end == bytes) {
                     break;
                 }
                 //check for 2 newlines in a row
@@ -142,19 +182,19 @@ int wrap(unsigned width, int input_fd, int output_fd) {
             }
             
             wordLength = end - start;
-
+            
+            if (wordLength > width) {
+                write(output_fd, "\n", 1);
+                write(output_fd, &buffer[start], wordLength);
+                // write(output_fd, "\n", 1);
+                error = 1;
+            }
             //end pointer reached end of buffer so store partial word
-            if (end >= size) {
+            else if (end == size) {
                 word = malloc((wordLength+1)*sizeof(char));
                 memcpy(word, &buffer[start], wordLength);
                 word[wordLength] = '\0';
                 break;
-            }
-            else if (wordLength > width) {
-                write(output_fd, "\n", 1);
-                write(output_fd, &buffer[start], wordLength);
-                characters += wordLength;
-                error = 1;
             }
             else if (start != end) {
                 if (characters + wordLength <= width) {
@@ -181,145 +221,14 @@ int wrap(unsigned width, int input_fd, int output_fd) {
             }
             start = end + 1;
             end = start;
-            if (end >= size) {
+            if (end == size) {
                 break;
             }
         }
 
     }
-    write(output_fd, "\n", 1);
+    
     free(buffer);
     if (error == 1) return 1;
     return 0;
-}
-
-int is_directory(char *name) {
-	struct stat data;
-	int err = stat(name, &data);
-	// should confirm err == 0
-	if (err) {
-		perror("Error.");
-		exit(EXIT_FAILURE);
-	}
-	if (S_ISDIR(data.st_mode)) {
-		// S_ISDIR macro is true if the st_mode says the file is a directory
-		// S_ISREG macro is true if the st_mode says the file is a regular file
-		return 1;
-	}
-	return 0;
-}
-
-int process_de(char* dir_name, char* de, unsigned page_width) {
-	// page width and filename given
-	char file_name[100] = "";
-	snprintf(file_name, sizeof(file_name), "%s/%s", dir_name, de);
-	int fd = open(file_name, O_RDONLY);
-	if (fd < 0) {
-		perror("Error: File does not exist.");
-		exit(EXIT_FAILURE);
-	}
-	char de_wrap[100] = "";
-	snprintf(de_wrap, sizeof(de_wrap), "%s/wrap.%s", dir_name, de);
-	int fd_wrap = open(de_wrap, O_WRONLY|O_TRUNC|O_CREAT, S_IRWXU);
-	int error = wrap(page_width, fd, fd_wrap);
-	if (error == 1) {
-		close(fd_wrap);
-		close(fd);
-		perror("Error: Page width exceeded.");
-		exit(EXIT_FAILURE);
-	}
-	close(fd_wrap);
-	close(fd);
-	return EXIT_SUCCESS;
-}
-
-int process_directory(char* dir_name, unsigned page_width) {
-	DIR* directory_p = opendir(dir_name);
-	struct dirent* directory_entry_p;
-	while ((directory_entry_p = readdir(directory_p))) {
-		//puts directory_entry -> directory_name
-		char* de= directory_entry_p->d_name;
-		//check for . and ..
-		if (!((strcmp(de, ".") == 0) || (strcmp(de, "..") == 0))) {
-			//check that file does not begin with "wrap"
-			if (strlen(de) > 3) {
-				char sub[5] = "";
-				strncpy(sub, de, 4);
-				if (!(strcmp(sub,"wrap") == 0)) {
-					process_de(dir_name, de, page_width);
-				}
-			}
-			else {
-				process_de(dir_name, de, page_width);
-			}
-		}
-	}
-	if (closedir(directory_p) == -1) {
-		perror("Error: Unable to close directory.");
-		exit(EXIT_FAILURE);
-	}
-	return EXIT_SUCCESS;
-}
-
-int process_file(char* file_name, unsigned page_width) {
-	// page width and filename given
-	int fd = open(file_name, O_RDONLY);
-	if (fd < 0) {
-		perror("Error: File does not exist.");
-		exit(EXIT_FAILURE);
-	}
-	int error = wrap(page_width, fd, 1);
-	if (error == 1) {
-		close(fd);
-		perror("Error: Page width exceeded.");
-		exit(EXIT_FAILURE);
-	}
-	close(fd);
-	return EXIT_SUCCESS;
-}
-
-int process_standard_input(unsigned page_width) {
-	//page width was given but no filename- read from standard input
-	int error = wrap(page_width, 0, 1);
-	if (error == 1) {
-		perror("Error: Page width exceeded.");
-		exit(EXIT_FAILURE);
-	}
-	return EXIT_SUCCESS;
-}
-
-int check_input(int argc, char** argv) {
-	unsigned page_width = atoi(argv[1]);
-	// check page width is valid
-	if (page_width <= 0) {
-			perror("Error: Page width invalid.");
-			exit(EXIT_FAILURE);
-	}
-	if (argc < 3) {
-		return process_standard_input(page_width);
-	}
-	else {
-		char* name = argv[2];
-		// check if directory or file
-		if (is_directory(name) == 1) {
-			return process_directory(name, page_width);
-		}
-		else {
-			return process_file(name, page_width);
-		}
-	}
-}
-
-int process_arguments(int argc, char** argv) {
-	// Check number of arguments passed
-	if (argc < 2){
-		// no arguments were passed
-		perror("Error: No arguments passed.");
-		exit(EXIT_FAILURE);
-	}
-	return check_input(argc, argv);
-}
-
-int main(int argc, char **argv) {
-	return process_arguments(argc, argv);
 }
